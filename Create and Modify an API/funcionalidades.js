6d60dfa82ea995c46c49d397c1030b5dbe992feb
Gaston <gastonfauret@gmail.com>
2023-06-05 12:13:39 -0300

    Modal CSS Style Done

--- Create and Modify an API/funcionalidades.js
+++ Create and Modify an API/funcionalidades.js
@@ -1,5 +1,20 @@
 const openModal = document.querySelector('#open-modal');
 
+const name = document.querySelector('#name');
+const lastName = document.querySelector('#lastName');
+const birth = document.querySelector('#birth');
+const gender = document.querySelector('#gender');
+const address = document.querySelector('#address');
+const city = document.querySelector('#city');
+const email = document.querySelector('#email');
+const phone = document.querySelector('#phone');
+
+const btnSubmit = document.querySelector('#btn-submit')
+
 openModal.addEventListener('click', () => {
     console.log('Boton Modal Anda Perfecto');
+})
+
+btnSubmit.addEventListener('click', () => {
+    console.log('Boton Submit Anda Perfecto');
 })
